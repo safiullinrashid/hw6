3fc1bac66064fa8035f91bac7512b2a42fe0a1fc
safiullinrashid <112946519+safiullinrashid@users.noreply.github.com>
2023-12-21 17:08:13 +0300

    hw6
    hw6

---
+++ Hw6/hw6-main/exercises/1/index.ts
@@ -0,0 +1,61 @@
+/*
+
+    Things to cover:
+
+        1. Basic typing.
+        2. Refining types.
+        3. Union types.
+        4. Merged types.
+        5. Generics.
+        6. Type declarations.
+        7. Module augmentation.
+        8. Advanced type mapping.
+
+    Rules and principles:
+
+        1. Avoid using "any" type at all costs.
+        2. Difficulty quickly grows one exercise after another.
+        3. Enjoy.
+
+Intro:
+
+    We are starting a small community of users. For performance
+    reasons we have decided to store all users right in the code.
+    This way we can provide our developers with more
+    user-interaction opportunities. With user-related data, at least.
+    All the GDPR-related issues will be solved some other day.
+    This would be the basis for our future experiments during
+    these exercises.
+
+Exercise:
+
+    Given the data, define the interface "User" and use it accordingly.
+
+*/
+export interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(`- ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
+// In case you are stuck:
+// https://www.typescriptlang.org/docs/handbook/2/objects.html
---
+++ Hw6/hw6-main/exercises/11/declarations/str-utils/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'str-utils' {
+    export const strReverse: (input: string) => string;
+    export const strToLower: (input: string) => string;
+    export const strToUpper: (input: string) => string;
+    export const strRandomize: (input: string) => string;
+    export const strInvertCase: (input: string) => string;
+}
